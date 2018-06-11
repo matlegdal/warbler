e2b87e727309dc35427507e7e3a8fc9d526699c8
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-06-11 09:11:10 -0400

    add instructions for windows machines

--- warbler-server/README.md
+++ warbler-server/README.md
@@ -1,8 +1,15 @@
 # warbler
 
-##Use
+##Usage
 ```
 clone the repo
 npm install
 npm start
 ```
+
+On mac/linux set npm start script to:
+```
+DEBUG=express:* nodemon index.js
+```
+On Windows set npm start script to:
+set DEBUG=express:* & nodemon index.js
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -5,8 +5,8 @@ mongoose.set('debug', true)
 mongoose.Promise = Promise
 
 mongoose.connect(`mongodb://${process.env.MONGOHOST}/${process.env.DATABASENAME}`, {
-  keepAlive: true
-})
+    keepAlive: true
+  })
 
 module.exports.User = require('./user')
 module.exports.Message = require('./message')
